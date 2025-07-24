349aac748e6f47a7485061de4630d806210d44de
Gloria Enyi <nkemaconam@gmail.com>
2025-07-24 19:28:02 +0100

    Upload index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ class malePerson {
   constructor(name, age) {
     this.name = name;
 this.age = age;
-introduce: function () {
+hello: function () {
         console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`);
   }
 }
@@ -13,7 +13,7 @@ class Student extends Person {
   constructor(name, age, grade) {
     super(name, age);
 this.grade = grade;
-study: function () {
+about: function () {
          console.log(`${this.name} is ${this.age} in grade ${this.grade}.`);
   }
   }
@@ -25,19 +25,19 @@ class Teacher extends Person {
     super(name, age);
     this.subject = subject;
 this.yearsOfExperience = yearsOfExperience;
-teach: function () {
+job: function () {
              console.log(`${this.name} is ${this.age} teaching ${this.subject} with ${this.yearsOfExperience} years of experience.`);
   }
   }
 }
 
 
-const person1 = new malePerson("Alex", 40);
-person1.introduce();
+const person1 = new malePerson("Gloria", 40);
+person1.hello();
 
-const student1 = new Student("Lily", 14, 9);
-student1.introduce();
-student1.study();
+const student1 = new Student("Amelia", 14, 9);
+student1.hello();
+student1.about();
 
-const teacher1 = new Teacher("Mr. Smith", 35, "Science", 10);
-teacher1.teach();
+const teacher1 = new Teacher("Sensei", 35, "History", 10);
+teacher1.job();
